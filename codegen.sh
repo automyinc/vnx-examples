9cbcab7a504475cdaad4a39763684a4b2210fe80
Max Wittal <automyinc@gmail.com>
2018-12-11 11:49:10 +0100

    update for new include paths

--- codegen.sh
+++ codegen.sh
@@ -4,5 +4,5 @@ VNX_INTERFACE_DIR=${VNX_INTERFACE_DIR:-/usr/interface}
 
 cd $(dirname "$0")
 
-vnxcppcodegen --cleanup generated/ example interface/ modules/ ${VNX_INTERFACE_DIR}/vnx/ ${VNX_INTERFACE_DIR}/basic/ ${VNX_INTERFACE_DIR}/math/
+vnxcppcodegen --cleanup generated/ example interface/ modules/ ${VNX_INTERFACE_DIR}/vnx/ ${VNX_INTERFACE_DIR}/automy/basic/ ${VNX_INTERFACE_DIR}/automy/math/
 
